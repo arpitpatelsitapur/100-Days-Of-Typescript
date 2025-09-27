ba5a9af2a3625097f819e63656423275b1010736
arpitpatel <arpitaa9918@gmail.com>
2025-09-27 13:41:59 +0530

    Delete dist directory

--- dist/day10.js
+++
@@ -1,6 +0,0 @@
-const btn = document.getElementById("btn");
-btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", () => {
-    console.log("Button clicked");
-});
-export {};
-//# sourceMappingURL=day10.js.map
